f28e9c10402a196aa9b2d145947e3f171003401f
Bassel Abdulsabour <basselabdulsabour@pop-os.localdomain>
2022-03-07 21:28:51 +0200

    Change download directory

--- scraping.py
+++ scraping.py
@@ -1,4 +1,3 @@
-from asyncio import streams
 from pytube import YouTube
 from pytube.cli import on_progress
 
@@ -7,5 +6,9 @@ class Scraping:
     def download(self, link):
         yt = YouTube(link)
         stream = yt.streams.get_audio_only()
-        stream.download()
+        try:
+            stream.download('/run/user/1000/gvfs/mtp:host=SAMSUNG_SAMSUNG_Android_RZ8NB0PBNWT/Internal storage/Audiobooks/YT-Audio')
+        except:
+            stream.download()
+
         self.tilte = yt.title
