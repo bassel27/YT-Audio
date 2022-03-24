e3a2eb7182d88e52826b9bd0be98cf04a18b66c2
Bassel Abdulsabour <basselabdulsabour@pop-os.localdomain>
2022-03-24 21:39:54 +0200

    download to different folders

--- scraping.py
+++ scraping.py
@@ -2,13 +2,24 @@ from pytube import YouTube
 from pytube.cli import on_progress
 
 class Scraping:
-    
-    def download(self, link):
+
+    def getFolder(option):
+        if option == "YT-Audio":
+            folder = '/run/user/1000/gvfs/mtp:host=SAMSUNG_SAMSUNG_Android_RZ8NB0PBNWT/Internal storage/Audiobooks/YT-Audio'
+        elif option == "Music":
+            folder = '/home/basselabdulsabour/Music to Help Study'
+        elif option == "Podcast":
+            folder = '/run/user/1000/gvfs/mtp:host=SAMSUNG_SAMSUNG_Android_RZ8NB0PBNWT/Internal storage/Audiobooks/Podcasts'
+        return folder
+
+    def download(self, link, option):
         yt = YouTube(link)
+        self.title = yt.title
         stream = yt.streams.get_audio_only()
         try:
-            stream.download('/run/user/1000/gvfs/mtp:host=SAMSUNG_SAMSUNG_Android_RZ8NB0PBNWT/Internal storage/Audiobooks/YT-Audio')
+            stream.download(Scraping.getFolder(option))
+
         except:
             stream.download()
 
-        self.tilte = yt.title
+    
--- tkinterClass.py
+++ tkinterClass.py
@@ -12,23 +12,37 @@ class Tkinter:
         link = self.entryLink.get()
         return link
 
+    def createOptionMenuType(self):
+        self.optionVar = StringVar()
+        self.optionVar.set("YT-Audio") 
+        optionMenuType = ttk.OptionMenu(self.frameInput, self.optionVar, "YT-Audio", "Podcast", "Music")
+        optionMenuType.grid(row = 2, column=0)
+
+    def getOption(self):
+        option = self.optionVar.get()
+        return option
+
     def frameInput(self):
-        frameInput = Frame(self.root)    #ttk.frame(self.root)
-        frameInput.pack()
+        self.frameInput = Frame(self.root)    #ttk.frame(self.root)
+        self.frameInput.pack()
 
-        Label(frameInput, text = "Enter youtube link").grid(row = 0, column = 0)
+        Label(self.frameInput, text = "Enter youtube link").grid(row = 0, column = 0)
 
-        self.entryLink = ttk.Entry(frameInput)
+        self.entryLink = ttk.Entry(self.frameInput)
         self.entryLink.grid(row = 1, column = 0, ipadx = 300)
+
+        self.createOptionMenuType()
 
         def clickEnter():
             scraping = Scraping()
-            thread = Thread(target = scraping.download, args = (self.getLink(),))
+            thread = Thread(target = scraping.download, args = (self.getLink(), self.getOption(),))
             self.entryLink.delete(0, END) 
             thread.start()
+            
 
-        buttonDownload = ttk.Button(frameInput, text = "Enter", command = clickEnter)
-        buttonDownload.grid(row = 2, column = 0)
+        buttonDownload = ttk.Button(self.frameInput, text = "Enter", command = clickEnter)
+        buttonDownload.grid(row = 3, column = 0)
+
 
     
 
